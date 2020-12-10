5e6a8a76ce1881eeda4637212f14c5f9c9533206
April Darger <april.m.darger@gmail.com>
2020-12-10 16:16:31 -0700

    seeds and migrations made and displaying in SQLiteStudio.

---
+++ data/seeds/001_recipes.js
@@ -0,0 +1,14 @@
+
+exports.seed = function(knex) {
+  // we want to remove all data before seeding
+  // truncate will reset the primary key each time
+  return knex('recipes').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('recipes').insert([
+        {recipe_id: 1, recipe_name: 'Candy Apples'},
+        {recipe_id: 4, recipe_name: 'Brownies'},
+        {recipe_id: 3, recipe_name: 'Pork Chops'}
+      ]);
+    });
+};
