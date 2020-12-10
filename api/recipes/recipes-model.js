e0cc1405bb1aa4b8e830778ad5d7a2e37314eef6
April Darger <april.m.darger@gmail.com>
2020-12-10 16:55:54 -0700

    Wrote db helpers per README.md instructions.

---
+++ api/recipes/recipes-model.js
@@ -0,0 +1,40 @@
+const db = require('../../data/db-config');
+
+module.exports = {
+  getRecipes,
+  getShoppingList,
+  getInstructions
+}
+
+function getRecipes() {
+  return db('recipes');
+}
+function getShoppingList(recipe_id) {
+// select
+//   si.ingredient_amount,
+//   i.ingredient_name
+// from step_ingredients si
+// join ingredients i
+//   on si.ingredient_id = i.ingredient_id
+// join steps s
+//   on si.step_id = s.step_id
+// join recipes r 
+//   on s.recipe_id = r.recipe_id
+// where r.recipe_id = 1;
+
+   return db('step_ingredients as si')
+    .join('ingredients as i', 'si.ingredient_id', 'i.ingredient_id')
+    .join('steps as s', 'si.step_id', 's.step_id')
+    .join('recipes r', 's.recipe_id', 'r.recipe_id')
+    .select('si.ingredient_amount', 'i.ingredient_name')
+    .where('r.recipe_id', recipe_id)
+}
+function getInstructions(recipe_id) {
+// select
+//  s.step_order,
+//  s.step_text
+// from steps as s
+// where s.recipe_id = 1;
+
+return db('steps as s').select('s.step_order', 's.step_text').where('s.recipe_id', recipe_id)
+}
--- data/seeds/001_recipes.js
+++ data/seeds/001_recipes.js
@@ -7,7 +7,7 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('recipes').insert([
         {recipe_id: 1, recipe_name: 'Candy Apples'},
-        {recipe_id: 4, recipe_name: 'Brownies'},
+        {recipe_id: 2, recipe_name: 'Brownies'},
         {recipe_id: 3, recipe_name: 'Pork Chops'}
       ]);
     });
