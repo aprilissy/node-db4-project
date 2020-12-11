55bbb320e41315d46aa54d1d7c4f3fc36aed33c7
April Darger <april.m.darger@gmail.com>
2020-12-10 17:42:33 -0700

    readability.

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -9,7 +9,15 @@ module.exports = {
 function getRecipes() {
   return db('recipes');
 }
+
 function getShoppingList(recipe_id) {
+  return db('step_ingredients as si')
+  .join('ingredients as i', 'si.ingredient_id', 'i.ingredient_id')
+  .join('steps as s', 'si.step_id', 's.step_id')
+  .join('recipes r', 's.recipe_id', 'r.recipe_id')
+  .select('si.ingredient_amount', 'i.ingredient_name')
+  .where('r.recipe_id', recipe_id)
+
 // select
 //   si.ingredient_amount,
 //   i.ingredient_name
@@ -21,20 +29,14 @@ function getShoppingList(recipe_id) {
 // join recipes r 
 //   on s.recipe_id = r.recipe_id
 // where r.recipe_id = 1;
-
-   return db('step_ingredients as si')
-    .join('ingredients as i', 'si.ingredient_id', 'i.ingredient_id')
-    .join('steps as s', 'si.step_id', 's.step_id')
-    .join('recipes r', 's.recipe_id', 'r.recipe_id')
-    .select('si.ingredient_amount', 'i.ingredient_name')
-    .where('r.recipe_id', recipe_id)
 }
+
 function getInstructions(recipe_id) {
+  return db('steps as s').select('s.step_order', 's.step_text').where('s.recipe_id', recipe_id)
+
 // select
 //  s.step_order,
 //  s.step_text
 // from steps as s
 // where s.recipe_id = 1;
-
-return db('steps as s').select('s.step_order', 's.step_text').where('s.recipe_id', recipe_id)
 }
--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -4,4 +4,14 @@ const Recipes = require('./recipes-model');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  Recipes.find()
+    .then(recipes => {
+      res.json(recipes);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get recipes' });
+    });
+});
+
 module.exports = router;
